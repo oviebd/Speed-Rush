7d36dbd13f4f05bbf9239df58e3e1ca47cd98bcb
oviebd <oviecse48@gmail.com>
2020-12-03 21:47:39 +0600

    Reward Implemented

--- Assets/Test game/Scripts/Platform/PlatformGenerator.cs
+++ Assets/Test game/Scripts/Platform/PlatformGenerator.cs
@@ -76,6 +76,7 @@ public class PlatformGenerator : MonoBehaviour
 		platformQueue.Enqueue(platformObj);
 
 		EnemyGenerator.instance.GenerateEnemy();
+		RewardGenerator.instance.GenerateReward();
 	}
 
 	private GameObject GetRandomPlatformPrefab()
---
+++ Assets/Test game/Scripts/Reward/RewardGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardGenerator : MonoBehaviour
+{
+	[HideInInspector]
+	public static RewardGenerator instance;
+
+	[SerializeField] private List<GameObject> rewardPrefabList;
+	[SerializeField] private GameObject rewardParent;
+
+	
+
+	private int enemyNumber = 1;
+	private int lastEnemyZpos = 0;
+
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+	}
+
+	public void GenerateReward()
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			GameObject enemyObj = InstantiateReward();
+		}
+
+	
+	}
+	private GameObject InstantiateReward()
+	{
+		GameObject enemyPrefab = GetRandomRewardPrefab();
+		GameObject enemyObj = Instantiate(enemyPrefab, rewardParent.transform);
+		enemyObj.name = "Reward " + enemyNumber;
+		enemyObj.transform.parent = null;
+
+		enemyObj.transform.position = GetRandomEnemyPosition();
+		enemyObj.transform.SetParent(rewardParent.transform, false);
+		enemyNumber = enemyNumber + 1;
+		return enemyObj;
+	}
+
+	private Vector3 GetRandomEnemyPosition()
+	{
+		Vector3 randomPosition = Vector3.zero;
+
+		float randomXpos = Random.Range(-4f, 4f);
+		int randomDistance = Random.Range(10, 20);
+		int zPos = lastEnemyZpos + randomDistance;
+
+		randomPosition = new Vector3(randomXpos, 0, zPos);
+
+		lastEnemyZpos = (int)randomPosition.z;
+		return randomPosition;
+	}
+
+	private GameObject GetRandomRewardPrefab()
+	{
+		int length = rewardPrefabList.Count;
+		int randomIndex = Random.Range(0, length);
+		GameObject platformPrefab = rewardPrefabList[randomIndex];
+		return platformPrefab;
+	}
+}
