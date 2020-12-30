d18f0e61f29287989f8574f823b984c386fb5839
oviebd <oviecse48@gmail.com>
2020-12-30 22:28:28 +0600

    Add Google Play Service Plugin

---
+++ Assets/Test game/Scripts/Play Service/GooglePlayServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class GooglePlayServiceManager : MonoBehaviour
+{
+
+	private void Start()
+	{
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+	}
+}
