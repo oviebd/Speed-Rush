249a318113ecffd36f2116523c82f31a1e8dbe87
oviebd <oviecse48@gmail.com>
2021-01-09 10:26:12 +0600

    implement overlay in UI

--- Assets/Test game/Scripts/UI/AnimatorPanel.cs
+++ Assets/Test game/Scripts/UI/AnimatorPanel.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimatorPanel : Panel
 {
 	protected string anim_identifier_Appearing = "isMenuAppearing";
 	[SerializeField] protected Animator animator;
 
+	private Image _backgroundImage;
+
+	private void Awake()
+	{
+		
+	}
+
+	private Image GetBackgroundImage()
+	{
+		if(_backgroundImage == null)
+			_backgroundImage = this.gameObject.GetComponent<Image>();
+		return _backgroundImage;
+
+	}
+
 	public override void Show()
 	{
 		base.Show();
+		GetBackgroundImage().enabled =true;
 		animator.SetBool(anim_identifier_Appearing, true);
 	}
 
 	public override void Hide()
 	{
 		animator.SetBool(anim_identifier_Appearing, false);
+		GetBackgroundImage().enabled = false;
 		CancelInvoke();
 		Invoke("HidePanel", 1.0f);
 	}
