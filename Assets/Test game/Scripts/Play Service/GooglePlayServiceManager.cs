7c7cb0e95e0f961dc8f04c022e70b32da366b9a3
tanvirfiji <58457826+tanvirfiji@users.noreply.github.com>
2025-07-12 17:48:04 +0600

    .

--- Assets/Test game/Scripts/Play Service/GooglePlayServiceManager.cs
+++ Assets/Test game/Scripts/Play Service/GooglePlayServiceManager.cs
@@ -1,101 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using GooglePlayGames;
 using GooglePlayGames.BasicApi;
-using GooglePlayGames;
 using UnityEngine;
 using SaveSystem;
 
 public class GooglePlayServiceManager : MonoBehaviour
 {
-	public static GooglePlayServiceManager instance;
+    public static GooglePlayServiceManager instance;
 
-	public delegate void OnPlayServiceSuccessfullyAuthenticated(PlayerDataModel userData);
-	public static event OnPlayServiceSuccessfullyAuthenticated onAuthenticatedSuccessfully;
+    public delegate void OnPlayServiceSuccessfullyAuthenticated(PlayerDataModel userData);
+    public static event OnPlayServiceSuccessfullyAuthenticated onAuthenticatedSuccessfully;
 
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
 
-	private void Awake()
-	{
-		if (instance == null)
-			instance = this;
-	}
+    private void Start()
+    {
+        // NO CONFIG NEEDED ANYMORE
+        PlayGamesPlatform.Activate();
 
-	private void Start()
-	{
-		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
-		PlayGamesPlatform.InitializeInstance(config);
-		PlayGamesPlatform.Activate();
+        SignIn();
+    }
 
-		SignIn();
-	}
+    void SignIn()
+    {
+        Social.localUser.Authenticate(success =>
+        {
+            PlayerDataModel data = new PlayerDataModel();
+            data.playerName = Social.localUser.userName;
+            data.playerID = Social.localUser.id;
+            UpdateUserWithHighScoreFromLeaderBoard(data);
+        });
+    }
 
-	void SignIn()
-	{
-		Social.localUser.Authenticate(success => {
-			PlayerDataModel data = new PlayerDataModel();
-			data.playerName = Social.localUser.userName;
-			data.playerID = Social.localUser.id;
-		   UpdateUserWithHighScoreFromLeaderBoard(data);
-		});
-	}
+    public void AddScoreToLeaderBoard(long score)
+    {
+        string leaderBoardId = GPGSIds.leaderboard_hall_of_honor;
 
-	public  void AddScoreToLeaderBoard(long score)
-	{
-		string leaderBordId = GPGSIds.leaderboard_hall_of_honor;
-	
-		Social.ReportScore(score, leaderBordId,success => {
-		});
-	}
+        Social.ReportScore(score, leaderBoardId, success => {
+            Debug.Log("Score reported: " + success);
+        });
+    }
 
-	public void ShowLeaderboardUi()
-	{
-		bool isNetworkAvilable = Utility.isNetworkAvilable();
-		if(isNetworkAvilable == true)
-		{
-			Social.ShowLeaderboardUI();
-		}
-		else
-		{
-			DialogClass alertDialogClass = new DialogBuilder().
-						 Title("Failed to connect with server").
-						 Message("Please check your network connection.").
-						 PositiveButtonText("Ok").
-						 PositiveButtonAction(ShowLeaderBoard).
-						 build();
+    public void ShowLeaderboardUi()
+    {
+        if (Utility.isNetworkAvilable())
+        {
+            Social.ShowLeaderboardUI();
+        }
+        else
+        {
+            DialogClass alertDialogClass = new DialogBuilder().
+                             Title("Failed to connect with server").
+                             Message("Please check your network connection.").
+                             PositiveButtonText("Ok").
+                             PositiveButtonAction(ShowLeaderBoard).
+                             build();
 
-			DialogManager.instance.SpawnDialogBasedOnDialogType(DialogTypeEnum.DialogType.AlertDialog, alertDialogClass);
-		}
-	}
+            DialogManager.instance.SpawnDialogBasedOnDialogType(DialogTypeEnum.DialogType.AlertDialog, alertDialogClass);
+        }
+    }
 
-	private void ShowLeaderBoard(IDialog iDialog)
-	{
-		iDialog.HideDialog();
-	}
+    private void ShowLeaderBoard(IDialog iDialog)
+    {
+        iDialog.HideDialog();
+    }
 
-	public long UpdateUserWithHighScoreFromLeaderBoard( PlayerDataModel user)
-	{
-		long highScore = -1;
-		Social.LoadScores(GPGSIds.leaderboard_hall_of_honor, scores =>
-		{
-			if (scores.Length > 0)
-			{
-				for (int i = 0; i < scores.Length; i++)
-				{
-					if (scores[i].userID == user.playerID)
-					{
-						highScore = scores[i].value;
-						user.highScore = highScore;
-						onAuthenticatedSuccessfully?.Invoke(user);
-						break;
-					}
-				}
-			}
-			else
-			{
-			}
-		});
+    public long UpdateUserWithHighScoreFromLeaderBoard(PlayerDataModel user)
+    {
+        long highScore = -1;
+        Social.LoadScores(GPGSIds.leaderboard_hall_of_honor, scores =>
+        {
+            if (scores.Length > 0)
+            {
+                for (int i = 0; i < scores.Length; i++)
+                {
+                    if (scores[i].userID == user.playerID)
+                    {
+                        highScore = scores[i].value;
+                        user.highScore = highScore;
+                        onAuthenticatedSuccessfully?.Invoke(user);
+                        break;
+                    }
+                }
+            }
+        });
 
-		return highScore;
-	}
-
-
+        return highScore;
+    }
 }
